6c790decd844910ea6d2c0652f02901c5bf9ba37
Tianruoxi <1040461147@qq.com>
2021-01-26 18:19:56 +0800

    完成页面结构

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <layout/>
+  </div>
+</template>
+
+<script>
+import Layout from './components/Layout'
+
+export default {
+  name: 'app',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style>
+
+</style>
